e799ddf9a7b88a795de4c5b05c5026545cd298c8
samruddhi1499 <samchavanxiv9@gmail.com>
2024-07-24 11:01:56 -0400

    Added validation to product category addition

--- src/main/java/com/example/ems_backend/dto/EmployeeDTO.java
+++ src/main/java/com/example/ems_backend/dto/EmployeeDTO.java
@@ -1,15 +1,53 @@
 package com.example.ems_backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+
 @NoArgsConstructor
-@AllArgsConstructor
+
 public class EmployeeDTO {
 
     private Long id;
+
+    public EmployeeDTO(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     private String firstName;
     private String lastName;
     private String email;
